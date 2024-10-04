f2754533c21f33f45c8c8a14b1ce18cae82cd17e
dihasananda <dihasananda@gmail.com>
2024-10-04 17:53:51 +0700

    add feature save to csv/excel

--- web-scraper-absen.py
+++ web-scraper-absen.py
@@ -1,7 +1,9 @@
 import requests
 import os
+import csv
 from dotenv import load_dotenv
 from bs4 import BeautifulSoup
+# import pandas as pd
 
 load_dotenv()
 
@@ -9,8 +11,8 @@ fin = []
 url_login = os.getenv('URL_LOGIN')
 url_scrape = os.getenv('URL_ABSEN')
 login_payload = {
-    "login_username": os.getenv('USERNAME'),
-    "login_password": os.getenv('PASSWORD')
+    'login_username': os.getenv('USERNAME'),
+    'login_password': os.getenv('PASSWORD')
 }
 
 kelas_asal = 22
@@ -22,28 +24,26 @@ with requests.session() as session:
     login_session = session.post(url_login, data=login_payload)
     cookies = login_session.cookies
 
-    while kelas_asal < 47: # 22 - 47
-        print("absen kelas : ", absen_kelas)
+    while kelas_asal < 48: # 22 - 47
         while jadwal_kegiatan < 13: # 8 - 12
             absen_number = absen_kelas
-            print("absen number : ", absen_number)
 
             absen_payload = {
-                "unit_sekolah_asal": 1,
-                "kelas_asal": kelas_asal, # 22 - 47
-                "jadwal_kegiatan": jadwal_kegiatan, # 8 - 12
-                "tanggal": "2024-10-03",
-                "cetak_harian": "PRINT",
-                "bulan": 10,
-                "tahun": 2024,
+                'unit_sekolah_asal': 1,
+                'kelas_asal': kelas_asal, # 22 - 47
+                'jadwal_kegiatan': jadwal_kegiatan, # 8 - 12
+                'tanggal': '2024-10-04',
+                'cetak_harian': 'PRINT',
+                'bulan': 10,
+                'tahun': 2024,
             }
 
             absen_session = session.post(url_scrape, cookies=cookies, data=absen_payload)
             absen_html = BeautifulSoup(absen_session.text, 'html.parser')
-            if 'putri' in absen_html.find(string="putri"):
-                continue
+            if 'Putri' in absen_html.get_text():
+                break
 
-            absen_table = absen_html.find(class_="gridtable")
+            absen_table = absen_html.find(class_='gridtable')
 
             for row in absen_table.find_all('tr'):
 
@@ -52,14 +52,26 @@ with requests.session() as session:
                     continue
                 data = [cell.text.strip() for cell in cells]
                 if jadwal_kegiatan == 8:
-                    fin.append(data[3:5])
-                else:
-                    fin[absen_number].append(data[4])
+                    fin.append([data[2].replace('Kelas ', '').replace(' - Putra', ''), data[3], 0])
+                if data[4] == 'Belum Presensi':
+                    fin[absen_number][2] += 1
 
                 absen_number += 1
             jadwal_kegiatan += 1
         jadwal_kegiatan = 8
         absen_kelas = absen_number
         kelas_asal += 1
-    print(fin)
+        print('load :', absen_kelas)
 
+with open('/mnt/c/Users/Asus/Desktop/data.csv', 'w') as f:
+     
+    # using csv.writer method from CSV package
+    write = csv.writer(f)
+     
+    write.writerow(['kelas', 'nama', 'absen'])
+    write.writerows(fin)
+
+# df = pd.DataFrame(fin)
+# writer = pd.ExcelWriter('/mnt/c/Users/Asus/Desktop/test.xlsx', engine='xlsxwriter')
+# df.to_excel(writer, index=False)
+# writer.save()
